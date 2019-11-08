10f4bc59df371ea5372382d97215b9c9987636f6
samurai365dev <57503749+samurai365dev@users.noreply.github.com>
2019-11-08 14:08:59 -0600

    Add files via upload

---
+++ UserScripts/PowerShellAdmins.ps1
@@ -0,0 +1,12 @@
+﻿$AUs = Get-Content "E:\UserScripts\UsersAU.txt"
+$Nonadmins = Get-Content "E:\UserScripts\UsersNA.txt"
+foreach ($Nonadmin in $Nonadmins) {
+
+        Remove-LocalGroupMember -Group "Administrators" -member $Nonadmin
+    }
+
+foreach ($AU in $AUs) {
+    
+    Remove-LocalGroupMember -Group "Power Users" -member $AU
+    Remove-LocalGroupMember -Group "Backup Operators" -member $AU
+    }
