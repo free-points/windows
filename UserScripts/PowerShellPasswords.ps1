dea7b15e3cf72c9846a92b579e0663a0748f0b6f
samurai365dev <57503749+samurai365dev@users.noreply.github.com>
2019-11-08 13:59:16 -0600

    Add files via upload

---
+++ UserScripts/PowerShellPasswords.ps1
@@ -0,0 +1,7 @@
+﻿$users = Get-Content "E:\UserScripts\UsersP.txt"
+foreach ($user in $users) {
+    
+    $UserAccount = Get-LocalUser -Name "$user"
+    $UserAccount | Set-LocalUser -Password (ConvertTo-SecureString -AsPlaintext "@aBc123!" -Force)
+        
+}
